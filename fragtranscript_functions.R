16be8578b49e8453e892f4c48781bbb1cac0dca6
LoayJabre <34510226+LoayJabre@users.noreply.github.com>
2018-07-04 13:59:46 -0300

    volcano plots, new line plots

--- fragtranscript_functions.R
+++ fragtranscript_functions.R
@@ -23,9 +23,8 @@ heatmap_unclustered <- function (group, dataset, proteinname){
 
 
 #x,y plots of the different proteins
-xyplot <- function(dataset,title, yzoom){
+xyplot <- function(dataset,title, yzoom1,yzoom2){
   title <- title
-  yzoom <- yzoom
   #simple x-yplot 
   print (ggplot(dataset, aes(x=variable, y=value, colour = name ))+
     geom_point(alpha=0.9)+
@@ -62,19 +61,21 @@ xyplot <- function(dataset,title, yzoom){
     theme_bw()+
     theme(plot.title = element_text(hjust = 0.5))+
     theme(axis.text.x = element_text(angle = 50, vjust = 1.0, hjust = 1.0))+
-    scale_y_continuous(limits = c(0,yzoom))+
+    scale_y_continuous(limits = c(yzoom1,yzoom2))+
     theme(legend.position = "none"))
+  
+  
 } 
 
 
 
 #this function creates subsets of the flavodoxin data and plots them as needed 
 #the loop creates means of the three treatments
-fixdata <- function(dataset, proteinname, title, title2){
+fixdata <- function(dataset, proteinname, title, title2, yzoom1,yzoom2){
   heatmap_unclustered(data, dataset, proteinname)
   
   datasetmelt <-  melt (data = dataset,id.vars = "name", measure.vars = c(2:28))
-  xyplot(datasetmelt,title, 0.001)
+  xyplot(datasetmelt,title, yzoom1, yzoom2)
   
   colname <- "mean"
   i=0
@@ -93,8 +94,129 @@ fixdata <- function(dataset, proteinname, title, title2){
   
   datasetmeanmelt <-  melt (data = datasetmean,id.vars = "name", measure.vars = c(2:10)) 
   
-  xyplot(datasetmeanmelt,title2, 0.002)
+  xyplot(datasetmeanmelt,title2, yzoom1, yzoom2)
+}
+
+
+
+xyplots <- function(dataset, title, title2, yzoom1,yzoom2){
   
+  datasetmelt <-  melt (data = dataset,id.vars = "name", measure.vars = c(2:28))
+  xyplot(datasetmelt,title, yzoom1, yzoom2)
+  
+  colname <- "mean"
+  i=0
+  while (i < 9){
+    colname <- paste(colname, i)
+    col1 = 2+(i*3)
+    col2 = 4+(i*3)
+    dataset[[colname]] <- rowMeans( dataset[, c(col1:col2)] ) 
+    i <- i+1
+  }
+  
+  datasetmean <- dataset [-c(2:28)]
+  names(datasetmean) <- c("name", "T0", "B12_0.5C", "Fe_B12_0.5C","0.5C", "Fe_0.5C", "3C", "Fe_3C", "6C", "Fe_6C" )
+  
+  datasetmeanmelt <-  melt (data = datasetmean,id.vars = "name", measure.vars = c(2:10)) 
+  
+  xyplot(datasetmeanmelt,title2, yzoom1, yzoom2)
+}
+
+
+
+#This function creates means of the the three treatments and plots the log values 
+xyplotconcise <- function(dataset, title){
+  
+  datasetmelt <-  melt (data = dataset,id.vars = "name", measure.vars = c(2:28))
+  
+  colname <- "mean"
+  i=0
+  while (i < 9){
+    colname <- paste(colname, i)
+    col1 = 2+(i*3)
+    col2 = 4+(i*3)
+    dataset[[colname]] <- rowMeans( dataset[, c(col1:col2)] ) 
+    i <- i+1
+  }
+  
+  datasetmean <- dataset [-c(2:28)]
+  names(datasetmean) <- c("name", "T0", "B12_0.5C", "Fe_B12_0.5C","0.5C", "Fe_0.5C", "3C", "Fe_3C", "6C", "Fe_6C" )
+  
+  datasetmeanmelt <-  melt (data = datasetmean,id.vars = "name", measure.vars = c(2:10)) 
+  
+  
+  
+  print (ggplot(datasetmelt, aes(x=variable, y=value, colour = name ))+
+           geom_point(alpha=0.9)+
+           stat_summary(fun.y = mean, geom = "line", colour = "black", group =1, size = 1.25)+
+           stat_summary(fun.data =  mean_se, geom = "errorbar", colour = "black")+
+           xlab ("Treatmet")+
+           ggtitle(title)+
+           theme_bw()+
+           theme(plot.title = element_text(hjust = 0.5))+
+           theme(axis.text.x = element_text(angle = 50, vjust = 1.0, hjust = 1.0))+
+           theme(legend.position = "none"))
+  
+  
+  print(ggplot(datasetmeanmelt, aes(x=variable, y=value, colour = name ))+
+          geom_point(alpha=0.9)+
+          stat_summary(fun.y = mean, geom = "line", colour = "black", group =1, size = 1.25)+
+          stat_summary(fun.data =  mean_se, geom = "errorbar", colour = "black")+
+          xlab ("Treatmet")+
+          ylab ("Value(Log10)")+
+          ggtitle(title)+
+          theme_bw()+
+          theme(plot.title = element_text(hjust = 0.5))+
+          theme(axis.text.x = element_text(angle = 50, vjust = 1.0, hjust = 1.0))+
+          scale_y_log10()+
+          theme(legend.position = "none"))
+  }
+
+
+  
+xyplotsmalldata <- function(dataset, title){
+  
+  datasetmelt <-  melt (data = dataset,id.vars = "name", measure.vars = c(2:28))
+  
+  colname <- "mean"
+  i=0
+  while (i < 9){
+    colname <- paste(colname, i)
+    col1 = 2+(i*3)
+    col2 = 4+(i*3)
+    dataset[[colname]] <- rowMeans( dataset[, c(col1:col2)] ) 
+    i <- i+1
+  }
+  
+  datasetmean <- dataset [-c(2:28)]
+  names(datasetmean) <- c("name", "T0", "B12_0.5C", "Fe_B12_0.5C","0.5C", "Fe_0.5C", "3C", "Fe_3C", "6C", "Fe_6C" )
+  
+  datasetmeanmelt <-  melt (data = datasetmean,id.vars = "name", measure.vars = c(2:10)) 
+  
+  
+  
+  print (ggplot(datasetmelt, aes(x=variable, y=value, colour = name ))+
+           geom_point()+
+           geom_line(aes(group=name))+
+           xlab ("Treatmet")+
+           ggtitle(title)+
+           theme_bw()+
+           theme(plot.title = element_text(hjust = 0.5))+
+           theme(axis.text.x = element_text(angle = 50, vjust = 1.0, hjust = 1.0))+
+           theme(legend.position = "none"))
+  
+  
+  print(ggplot(datasetmeanmelt, aes(x=variable, y=value, colour = name ))+
+          geom_point()+
+          geom_line(aes(group=name))+
+          xlab ("Treatmet")+
+          ylab ("Value(Log10)")+
+          ggtitle(title)+
+          theme_bw()+
+          theme(plot.title = element_text(hjust = 0.5))+
+          theme(axis.text.x = element_text(angle = 50, vjust = 1.0, hjust = 1.0))+
+          scale_y_log10()+
+          theme(legend.position = "none"))
 }
 
 
